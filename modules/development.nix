6b3fddf3f45abe2f6a658655da0266150ffa2490
Gakawarstone <pinnekerv@gmail.com>
2026-08-09 19:04:25 +0200

    feat(packages): install herdr binary

--- modules/development.nix
+++ modules/development.nix
@@ -1,6 +1,32 @@
 { pkgs, pkgsUnstable, ... }:
 
 let
+  herdr = pkgs.stdenvNoCC.mkDerivation {
+    pname = "herdr";
+    version = "0.8.0";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/herdrdev/herdr/releases/download/v0.8.0/herdr-linux-x86_64";
+      hash = "sha256-uHLqfkD6LLF+hXrJtisb8m23tAPGIvXS8/WzX26azSg=";
+    };
+
+    dontUnpack = true;
+
+    installPhase = ''
+      runHook preInstall
+      install -Dm755 "$src" "$out/bin/herdr"
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Terminal workspace manager for AI coding agents";
+      homepage = "https://herdr.dev";
+      license = pkgs.lib.licenses.asl20;
+      mainProgram = "herdr";
+      platforms = [ "x86_64-linux" ];
+    };
+  };
+
   t3codeVersion = "0.0.32";
 
   t3code = pkgs.appimageTools.wrapType2 {
@@ -29,6 +55,7 @@ in
     uv
     pkgsUnstable.opencode
     pkgsUnstable.codex
+    herdr
     lazygit
     t3code
     t3codeDesktop
